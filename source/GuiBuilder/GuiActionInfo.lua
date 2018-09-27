aa4c7ccb4e968625e476428cf56d7d6630903cca
kennethloeffler <kenneth.loeffler@outlook.com>
2018-09-27 09:16:56 -0700

    Add GuiActions SetSize, SetPosition, SetText, SetImage

--- source/GuiBuilder/GuiActionInfo.lua
+++ source/GuiBuilder/GuiActionInfo.lua
@@ -20,32 +20,26 @@ local GuiActionInfo = {
 			args.elementToToggle.Visible = not args.elementToToggle.Visible
 		end
 	},
-	TweenPosition = {
+	SetSize = {
 		name = "string",
-		ElementToTween = "instance",
-		EasingStyle = "string",
-		EasingDirection = "string",
-		RepeatCount = "number",
-		DelayTime = "number",
-		["Time"] = "number",
+		Element = "instance",
 		["xScale"] = "number",
 		["xOffset"] = "number",
 		["yScale"] = "number",
 		["yOffset"] = "number",
 		["func"] = function(args)
-			local tween = game:GetService("TweenService"):Create(
-				args.ElementToTween, 
-				TweenInfo.new(
-					args.Time, 
-					Enum.EasingStyle[args.EasingStyle], 
-					Enum.EasingDirection[args.EasingDirection],
-					args.RepeatCount,
-					false,
-					args.DelayTime
-				),
-				{Position = UDim2.new(args.xScale, args.xOffset, args.yScale, args.yOffset)}
-			)
-			tween:Play()
+			args.Element.Size = UDim2.new(args.xScale, args.xOffset, args.yScale, args.yOffset)
+		end
+	},
+	SetPosition = {
+		name = "string",
+		Element = "instance",
+		["xScale"] = "number",
+		["xOffset"] = "number",
+		["yScale"] = "number",
+		["yOffset"] = "number",
+		["func"] = function(args)
+			args.Element.Position = UDim2.new(args.xScale, args.xOffset, args.yScale, args.yOffset)
 		end
 	},
 	TweenSize = {
@@ -76,6 +70,34 @@ local GuiActionInfo = {
 			tween:Play()
 		end
 	},
+	TweenPosition = {
+		name = "string",
+		ElementToTween = "instance",
+		EasingStyle = "string",
+		EasingDirection = "string",
+		RepeatCount = "number",
+		DelayTime = "number",
+		["Time"] = "number",
+		["xScale"] = "number",
+		["xOffset"] = "number",
+		["yScale"] = "number",
+		["yOffset"] = "number",
+		["func"] = function(args)
+			local tween = game:GetService("TweenService"):Create(
+				args.ElementToTween, 
+				TweenInfo.new(
+					args.Time, 
+					Enum.EasingStyle[args.EasingStyle], 
+					Enum.EasingDirection[args.EasingDirection],
+					args.RepeatCount,
+					false,
+					args.DelayTime
+				),
+				{Position = UDim2.new(args.xScale, args.xOffset, args.yScale, args.yOffset)}
+			)
+			tween:Play()
+		end
+	},
 	SetBackgroundColor3 = {
 		name = "string",
 		Element = "instance",
@@ -86,14 +108,16 @@ local GuiActionInfo = {
 			args.Element.BackgroundColor3 = Color3.fromRGB(args.R, args.G, args.B)
 		end
 	},
-	SetImageColor3 = {
+	SetText = {
 		name = "string",
 		Element = "instance",
-		R = "number",
-		G = "number",
-		B = "number",
+		Text = "string",
 		["func"] = function(args)
-			args.Element.ImageColor3 = Color3.fromRGB(args.R, args.G, args.B)
+			if args.Element.Text then
+				args.Element.Text = args.Text
+			else
+				error("GuiBuilder: SetText 'Element' must be set to a TextLabel, TextButton, or TextBox")
+			end
 		end
 	},
 	SetTextColor3 = {
@@ -103,9 +127,39 @@ local GuiActionInfo = {
 		G = "number",
 		B = "number",
 		["func"] = function(args)
-			args.Element.TextColor3 = Color3.fromRGB(args.R, args.G, args.B)
+			if args.Element.Text then
+				args.Element.TextColor3 = Color3.fromRGB(args.R, args.G, args.B)
+			else
+				error("GuiBuilder: SetTextColor3 'Element' must be set to a TextLabel, TextButton, or TextBox")
+			end
 		end
-	}
+	},
+	SetImage = {
+		name = "string",
+		Element = "instance",
+		AssetId = "number",
+		["func"] = function(args)
+			if args.Element.Image then
+				args.Element.Image = "rbxassetid://".. tostring(args.AssetId)
+			else
+				error("GuiBuilder: SetImage 'Element' must be set to an ImageLabel or ImageButton")
+			end
+		end
+	},
+	SetImageColor3 = {
+		name = "string",
+		Element = "instance",
+		R = "number",
+		G = "number",
+		B = "number",
+		["func"] = function(args)
+			if args.Element.Image then
+				args.Element.ImageColor3 = Color3.fromRGB(args.R, args.G, args.B)
+			else
+				error("GuiBuilder: SetImageColor3 'Element' must be set to an ImageLabel or ImageButton")
+			end
+		end
+	},
 }
 
 return GuiActionInfo
